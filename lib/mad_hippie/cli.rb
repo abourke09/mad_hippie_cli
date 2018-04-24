eb344cb7f485832849fb50064416f88a0d49d6c5
Annie Bourke <aabourke@gmail.com>
2018-04-24 23:10:12 +0000

    stubs out menu and goodbye method functionality

--- lib/mad_hippie/cli.rb
+++ lib/mad_hippie/cli.rb
@@ -1,7 +1,10 @@
 class CLI
 
   def call
-    puts "Welcome to Mad Hippie Skincare!"
+    puts "Welcome to the Mad Hippie CLI!"
+    list_products
+    menu
+    goodbye
   end
 
   def list_products
@@ -12,4 +15,23 @@ class CLI
     DOC
   end
 
+  def menu
+    puts "Enter the number of the skincare item you would like to know more about, or type exit."
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "more info on product 1"
+      when "2"
+        puts "more info on product 2"
+      when "3"
+        puts "more infor on product 3"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thanks for visiting, come back anytime."
+  end
 end
