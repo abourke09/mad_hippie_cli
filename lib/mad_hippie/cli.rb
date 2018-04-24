5f8d19a8124d88a9ff308b13aa1aabb9f07ea640
Annie Bourke <aabourke@gmail.com>
2018-04-24 23:02:09 +0000

    creates new list_products method and stubs out functionality

--- lib/mad_hippie/cli.rb
+++ lib/mad_hippie/cli.rb
@@ -4,4 +4,12 @@ class CLI
     puts "Welcome to Mad Hippie Skincare!"
   end
 
+  def list_products
+    puts <<-DOC
+    1. Vitamin C Serum
+    2. Antioxidant Facial Oil
+    3. Exfoliating Serum
+    DOC
+  end
+
 end
