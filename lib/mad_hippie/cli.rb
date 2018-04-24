1616a7f33ccded89aea2a64759ec1931229391df
Annie Bourke <aabourke@gmail.com>
2018-04-24 23:17:17 +0000

    adds functionality to stubbed out menu method in cli class

--- lib/mad_hippie/cli.rb
+++ lib/mad_hippie/cli.rb
@@ -16,9 +16,9 @@ class CLI
   end
 
   def menu
-    puts "Enter the number of the skincare item you would like to know more about, or type exit."
     input = nil
     while input != "exit"
+      puts "Enter the number of the skincare item you would like to know more about. Alternatively, type 'list' to view all of the products again, or 'exit' to leave."
       input = gets.strip.downcase
       case input
       when "1"
@@ -27,6 +27,10 @@ class CLI
         puts "more info on product 2"
       when "3"
         puts "more infor on product 3"
+      when "list"
+        list_products
+      else
+        puts "I'm not sure what you mean."
       end
     end
   end
