3c0a3552b19a8caf08b11c9bdfc11ca788fb0138
Annie Bourke <aabourke@gmail.com>
2018-04-25 23:49:53 +0000

    rearranges the methods' contents to just get the darn thing working

--- lib/mad_hippie/scraper.rb
+++ lib/mad_hippie/scraper.rb
@@ -4,34 +4,38 @@ class Scraper
     @doc = Nokogiri::HTML(open("https://www.madhippie.com/collections/all-products"))
   end
 
-  def scrape_urls
-    urls = []
-    links = @doc.css("a.grid-link")
-    links.each {|link| urls << link["href"]}
-    urls
-    binding.pry
-  #  links.each {|link| Product.new(url = link["href"])}
-  end
-
   def scrape_products
+    @urls = []
+
+    links = @doc.css("a.grid-link")
+    links.each {|link| @urls << link["href"]}
     #take Product.all array of whatever it is and take out the URL for each
     #put that URL in Nokogiri
     #when scraping with Nokogiri, assign the name, price, and description for each Product instance.
     #whew! hope it works!!!!! :o
-    urls.each do |url|
-      full_link = "https://www.madhippie.com"+"url"
-      @new_doc = Nokogiri::HTML(open(full_link))
-      name = @new_doc.css("")
+    @urls.each do |url|
+      full_link = "https://www.madhippie.com"+"#{url}"
+      binding.pry
+      @product_page = Nokogiri::HTML(open(full_link))
+
+      name = @product_page.css("h1").text
       url = full_link
-      price = @new_doc.css("")
-      description = @new_doc.css("")
+      price = @product_page.css("#ProductPrice").text.strip
+      description = @product_page.css("p strong").text
+
       Product.new(name, url, price, description)
     end
-
-    Product.all.each do |item|
-      puts item.url
-    end
-
   end
+
+
+#
+#    def self.urls
+#      @urls
+#    end
+#
+#    def scrape_urls
+#      links = @doc.css("a.grid-link")
+#      links.each {|link| @urls << link["href"]}
+#    end
 
 end
