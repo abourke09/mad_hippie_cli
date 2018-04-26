18bc8dbabe1bb6ac9883db94966505d07f5238ec
Annie Bourke <aabourke@gmail.com>
2018-04-26 01:46:59 +0000

    refines and corrects errors in CLI class, removes comments in Product class

--- lib/mad_hippie/cli.rb
+++ lib/mad_hippie/cli.rb
@@ -7,10 +7,17 @@ class CLI
 
   def call
     puts "Welcome to the Mad Hippie CLI!"
+    puts ""
     list_products
     menu
     goodbye
   end
+
+#Q: How do I scrape my Descriptions better over all 13 products?
+#Q: How do I upload my screenrecording in order to link it to the Learn Curriculum page?
+#Q: Separation of concerns, clean up, ect
+#Q: Is there a better way to add line spacing, other than 'puts "" ' ?
+#Q: How do I indent the list of products? I tried using .indent(#) on the string but it didn't work.
 
   def list_products
     puts "Mad Hippie Skincare Line:"
@@ -22,16 +29,24 @@ class CLI
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the skincare item you would like to know more about. Alternatively, type 'list' to view all of the products again, or 'exit' to leave."
+      puts ""
+      puts "Enter the number of the skincare item you would like to know more about."
+      puts "Alternatively, type 'list' to view all of the products again, or 'exit' to leave."
+      puts ""
       input = gets.strip.downcase
 
       if input.to_i > 0
-        binding.pry
-        puts @products[input.to_i - 1]
+        x = input.to_i - 1
+        puts "Product: #{@products[x].name}"
+        puts "Price: #{@products[x].price}"
+        puts "Description: #{@products[x].description}"
+        puts ""
       elsif input == "list"
         list_products
+      elsif input == "exit"
+        break
       else
-        puts "I'm not sure what you mean."
+        puts "I'm not sure what you mean by that."
       end
 
     end
--- lib/mad_hippie/product.rb
+++ lib/mad_hippie/product.rb
@@ -15,49 +15,4 @@ class Product
     @@all
   end
 
-=begin
-  def self.all
-    products = [ ]
-
-  #  @urls = Scraper.scrape_urls
-  #  @urls.each {|url| products << url}
-
-    doc = Nokogiri::HTML(open("https://www.madhippie.com/collections/all-products"))
-    text = doc.css("p.grid-link__title")
-    text.each {|name| products << name.text}
-    products.delete("Mad Hippie")
-
-    products
-  #  item_1 = self.new
-  #  item_1.name = "Vitamin C Serum"
-  #  item_1.price = "$33.99"
-  #  item_1.url = "https://www.madhippie.com/collections/all-products/products/vitamin-c-serum"
-
-  #  item_2 = self.new
-  #  item_2.name = "Antioxidant Facial Oil"
-  #  item_2.price = "$24.99"
-  #  item_2.url = "https://www.madhippie.com/collections/all-products/products/antioxidant-facial-oil"
-
-  #  [item_1, item_2]
-  end
-
-  def scrape_products
-    products = [ ]
-
-    products << self.scrape_mad_hippie
-
-    products
-  end
-
-  def scrape_mad_hippie
-    products = [ ]
-
-    doc = Nokogiri::HTML(open("https://www.madhippie.com/collections/all-products"))
-    text = doc.css("p.grid-link__title")
-    text.each {|name| products << name.text}
-    products.delete("Mad Hippie")
-
-    return products
-  end
-=end
 end
