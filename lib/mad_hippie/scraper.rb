9c05c0d621fdef893c5906a40d02c39ea25ffe06
Annie Bourke <aabourke@gmail.com>
2018-04-30 16:12:27 +0000

    edit loop in scrape_products so that the new Product initialization takes a hash as an arguement

--- lib/mad_hippie/scraper.rb
+++ lib/mad_hippie/scraper.rb
@@ -15,13 +15,13 @@ class MadHippie::Scraper
     @urls.each do |url|
       full_link = "https://www.madhippie.com"+"#{url}"
       product_page = Nokogiri::HTML(open(full_link))
-
-      name = product_page.css("h1").text
-      price = product_page.css("#ProductPrice").text.strip
-      url = full_link
-      description = product_page.css("p strong").text
-
-      MadHippie::Product.new(name, price, url, description)
+      details = {
+        name: product_page.css("h1").text,
+        price: product_page.css("#ProductPrice").text.strip,
+        url: full_link,
+        description: product_page.css("p strong").text
+      }
+      MadHippie::Product.new(details)
     end
   end
 
