5b1f3b054eeaa510d734aef52a362c3c88b88b06
Annie Bourke <aabourke@gmail.com>
2018-04-30 15:24:02 +0000

    simplify initialize method by using send to enable mass attribute assignment

--- lib/mad_hippie/product.rb
+++ lib/mad_hippie/product.rb
@@ -3,12 +3,8 @@ class MadHippie::Product
 
   @@all = []
 
-  def initialize(name, price, url, description)
-    # attr_hash.each {|k,v| self.send("#{k}=", v)}
-    @name = name
-    @price = price
-    @url = url
-    @description = description
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
     @@all << self
   end
 
